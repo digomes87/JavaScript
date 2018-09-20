e3094c9f4a0aaea9fc22eb5edb4b2ca1947c3842
DiegoGo <DiegoGo@protonMail.com>
2018-09-20 15:02:58 -0300

    ES6 - JavaScript Mais Moderno
    O javascript atual usar novas varaiveis, como let e const, que foram inseridas na ultima versão do javascript. Como estou vendo JS basico na facu, resolvi catar e criar um mini repo de atualizações do js.

---
+++ JavaScript_Moderno_ES6/01/app.js
@@ -0,0 +1,15 @@
+
+var animal;
+
+function sayAnimal(){
+    var animal = 'car';
+    console.log(animal)
+}
+
+
+    sayAnimal();
+    console.log(animal);
+
+
+    let sound = 'jone';
+    const name = 'Nome';
